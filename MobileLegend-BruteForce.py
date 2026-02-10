55b5d69cbf79a026e0279b6fb53cb1502d65ec68
bisma <gyndarab@gmail.com>
2026-02-10 15:08:04 +0700

    first init

---
+++ MobileLegend-BruteForce.py
@@ -0,0 +1,143 @@
+import streamlit as st
+from streamlit_folium import st_folium
+import pandas as pd
+import matplotlib.pyplot as plt
+
+heroMage = pd.read_excel('data/hero_mage.xlsx')
+magicItem = pd.read_excel('data/item_magic.xlsx')
+
+st.subheader('Pilih Hero')
+selected_hero = st.selectbox(
+    '-',
+    heroMage['nama hero']
+)
+
+hero = heroMage[heroMage['nama hero'] == selected_hero].iloc[0]
+
+hero_type = hero['type hero'].strip().lower()
+hero_attack_type = hero['attack type'].strip().lower()
+
+nama_item = magicItem['nama item'].tolist()
+cd_item = magicItem['cooldown reduction'].tolist()
+power_item = magicItem['magic power'].tolist()
+speed_item = magicItem['attack speed'].tolist()
+
+limitItem = 3
+
+bestItemCount = 0
+bestCombo = ["", "", ""]
+bestCd = 0
+bestPower = 0
+bestSpeed = 0
+
+expHP = 3850
+limitSpeed = 20
+
+if hero_type == 'burst':
+    limitCd = 30
+elif hero_type == 'cd':
+    limitCd = 25
+
+jumlah_item = len(nama_item)
+
+for i in range(jumlah_item):
+    total_cd = cd_item[i]
+    total_power = power_item[i]
+    total_speed = speed_item[i]
+
+    if (hero_attack_type == 'basic attack'):
+        if (total_speed > 0) and (total_speed <= limitSpeed):
+            if (bestItemCount < 1) or (total_speed > bestSpeed) or (total_speed == bestSpeed) and (total_power > bestPower):
+                bestItemCount = 1
+                bestCd = total_cd
+                bestPower = total_power
+                bestSpeed = total_speed
+                bestCombo[0] = nama_item[i]
+                bestCombo[1] = ""
+                bestCombo[2] = ""
+
+    elif (hero_attack_type == 'skill'):
+        if (total_speed == 0) and (total_cd <= limitCd):
+            if (bestItemCount < 1) or (total_cd > bestCd) or (total_cd == bestCd) and (total_power > bestPower):
+                bestItemCount = 1
+                bestCd = total_cd
+                bestPower = total_power
+                bestSpeed = 0
+                bestCombo[0] = nama_item[i]
+                bestCombo[1] = ""
+                bestCombo[2] = ""
+
+for i in range(jumlah_item):
+    for j in range(i + 1, jumlah_item):
+
+        total_cd = cd_item[i] + cd_item[j]
+        total_power = power_item[i] + power_item[j]
+
+        speed_i = speed_item[i]
+        speed_j = speed_item[j]
+
+        if (hero_attack_type == 'basic attack'):
+            if (speed_i > 0) and (speed_j == 0) and (total_cd <= limitCd):
+                if (bestItemCount < 2) or (total_cd > bestCd) or (total_cd == bestCd) and (total_power > bestPower):
+                    bestItemCount = 2
+                    bestCd = total_cd
+                    bestPower = total_power
+                    bestSpeed = speed_i
+                    bestCombo[0] = nama_item[i]
+                    bestCombo[1] = nama_item[j]
+                    bestCombo[2] = ""
+
+        elif (hero_attack_type == 'skill'):
+            if (speed_i == 0) and (speed_j == 0) and (total_cd <= limitCd):
+                if (bestItemCount < 2) or (total_cd > bestCd) or (total_cd == bestCd) and (total_power > bestPower):
+                    bestItemCount = 2
+                    bestCd = total_cd
+                    bestPower = total_power
+                    bestSpeed = 0
+                    bestCombo[0] = nama_item[i]
+                    bestCombo[1] = nama_item[j]
+                    bestCombo[2] = ""
+
+for i in range(jumlah_item):
+    for j in range(i + 1, jumlah_item):
+        for k in range(j + 1, jumlah_item):
+
+            total_cd = cd_item[i] + cd_item[j] + cd_item[k]
+            total_power = power_item[i] + power_item[j] + power_item[k]
+
+            speed_i = speed_item[i]
+            speed_j = speed_item[j]
+            speed_k = speed_item[k]
+
+            if hero_attack_type == 'basic attack':
+                if (speed_i > 0) and (speed_j == 0) and (speed_k == 0) and (total_cd <= limitCd) or (cd_item[j] == 0 and cd_item[k] == 0 ):
+                    if (
+                        bestItemCount < 3 or
+                        total_cd > bestCd or
+                        (total_cd == bestCd and total_power > bestPower)
+                    ):
+                        bestItemCount = 3
+                        bestCd = total_cd
+                        bestPower = total_power
+                        bestSpeed = speed_i
+                        bestCombo[0] = nama_item[i]
+                        bestCombo[1] = nama_item[j]
+                        bestCombo[2] = nama_item[k]
+
+            elif (hero_attack_type == 'skill'):
+                if (speed_i == 0) and (speed_j == 0) and (speed_k == 0) and (total_cd <= limitCd) or(cd_item[i] == 0 and cd_item[j] == 0 and cd_item[k] == 0):
+                    if (bestItemCount < 3) or (total_cd > bestCd) or (total_cd == bestCd and total_power > bestPower):
+                        bestItemCount = 3
+                        bestCd = total_cd
+                        bestPower = total_power
+                        bestSpeed = 0
+                        bestCombo[0] = nama_item[i]
+                        bestCombo[1] = nama_item[j]
+                        bestCombo[2] = nama_item[k]
+
+st.write('Hero :', selected_hero)
+
+st.write('kombinasi item terpilih :')
+
+for item in bestCombo:
+    st.write('-', item)
